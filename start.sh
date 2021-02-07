d8eb2f0365536daddb0e2bbc87079cde92b8c879
fakhiralkda <69405341+fakhiralkda@users.noreply.github.com>
2021-02-07 07:06:22 +0700

    dump ginkgoeu
    Signed-off-by: rzlamrr <rizal82rebel@gmail.com>

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+apt -qq -y update
+apt -qq -y upgrade
+apt install -qq -y git curl wget
 
 git config --global user.email "rizal82rebel@gmail.com"
 git config --global user.name "rzlamrr"
