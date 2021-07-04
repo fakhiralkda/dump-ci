751452280d329cee4aa862978d6fae904ac148bb
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-07-05 06:55:04 +0700

    dumper: better way
    Signed-off-by: rzlamrr <rzlamrr.dvst@protonmail.com>

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "::group::Preparation"
+rm -rf .git
 git config --global user.email "rzlamrr.dvst@protonmail.com"
 git config --global user.name "rzlamrr"
+echo "::endgroup::"
 
-git clone https://github.com/DroidDumps/phoenix_firmware_dumper -q dumper
+echo "::group::Clone and set Env"
+git clone https://github.com/DroidDumps/phoenix_firmware_dumper dumper
 cd dumper || exit
 
 echo ${PAT} > .github_token
@@ -11,5 +15,17 @@ echo ${ORG} > .github_orgname
 echo ${TOKEN} > .tg_token
 echo "-1001437241144" > .tg_chat
 
+sed -i "s/[p][r][i][n][t][f][ ][\"][\][n][\][n][>][D][u][m][p][e][d][ ][b][y]/#/g" dumper*
+echo "::endgroup::"
+
+echo "::group::Setup"
 sudo ./setup.sh
+echo "::endgroup::"
+
+echo "::group::Dumpyara"
 ./dumper.sh $1
+echo "::endgroup::"
+
+echo "::group::Post Action"
+rm .github_* .tg_*
+echo "::endgroup::"
