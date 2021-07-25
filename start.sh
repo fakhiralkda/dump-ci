d0844fd6d10610687e6f9e5d8eff98ad8cecfc32
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2021-07-25 09:55:06 +0700

    start: split files larger than 50MB
    github recommendation is 50MB max file size

--- start.sh
+++ start.sh
@@ -15,7 +15,8 @@ echo ${ORG} > .github_orgname
 echo ${TOKEN} > .tg_token
 echo "-1001437241144" > .tg_chat
 
-sed -i "s/[p][r][i][n][t][f][ ][\"][\][n][\][n][>][D][u][m][p][e][d][ ][b][y]/#/g" dumper*
+sed -i "s/[p][r][i][n][t][f][ ][\"][\][n][\][n][>][D][u][m][p][e][d][ ][b][y]/#/g" dumper.sh
+sed -i "s/62M/50M/g" dumper.sh
 echo "::endgroup::"
 
 echo "::group::Setup"
@@ -27,5 +28,5 @@ echo "::group::Dumpyara"
 echo "::endgroup::"
 
 echo "::group::Post Action"
-rm .github_* .tg_*
+rm -rf .github_* .tg_* *
 echo "::endgroup::"
